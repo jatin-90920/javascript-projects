7a433eca6cea8aa325cdfd8fc66f7e3135525b49
Jatin <jatingoyal769@gmail.com>
2023-02-07 16:15:26 +0530

    This is the fix to back button

--- script.js
+++ script.js
@@ -94,5 +94,5 @@ async function movieshow(){
     // })
 }
 function redirect(){
-    window.location = "http://127.0.0.1:5500/index.html";
+    window.location = "https://jatin-90920.github.io/movie-search/";
 }
